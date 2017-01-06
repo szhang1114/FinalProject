64ffd29361f4b862b003575b3d72481ae142c8b8
Steven Zhang <szhang@stuy.edu>
2017-01-06 01:15:12 -0500

    buttons

--- GUItest/GUItest.java
+++ GUItest/GUItest.java
@@ -1,14 +1,37 @@
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.*;
+//import javax.swing.ImageIcon;
+ 
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+
 public class GUItest extends JFrame {
-    public Window() {
+    private JButton bWeather, bClock, b;
+    private Container pane;
+    public GUItest() {
         this.setTitle("First GUI test");
-        this.setSize(600,400);
-        this.setLocation(100,100);
+	this.setSize(100,100);
+	this.setLocation(100,100);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	
+	bWeather = new JButton("Weather");
+	
+	bClock = new JButton("Clock");
+
+	b = new JButton("asdf");
+
+	pane.add(bWeather);
+	pane.add(bClock);
+	//pane.add(b);
     }
 
     public static void main(String[] args) {
-        Window g = new Window();
+        GUItest g = new GUItest();
         g.setVisible(true);
     }
-}public class GUItest extends JFrame
+}
---
+++ GUItest/GUItest.java~
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class GUItest extends JFrame {
+    public Window() {
+        this.setTitle("First GUI test");
+        this.setSize(600,400);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        Window g = new Window();
+        g.setVisible(true);
+    }
+}public class GUItest extends JFrame
